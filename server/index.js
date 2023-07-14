587376ce92ea1c30dfb5f1e23ffc10a10e92e477
Namrata Balwani <namratabalwani1@gmail.com>
2023-07-14 18:19:15 -0400

    changes

--- server/index.js
+++ server/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.listen(5000, () => console.log("Server started"));
+app.get('/', console.log("WE GOT A REQUEST. YAYYYYY!"));
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
